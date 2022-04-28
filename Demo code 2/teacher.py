f24d75b4a2863d00718cb09bf44532d65ad30122
Longgang <73026617+dailonggang@users.noreply.github.com>
2022-04-28 23:32:58 +0800

    Create teacher.py

---
+++ Demo code 2/teacher.py
@@ -0,0 +1,113 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+import torch.utils.data
+
+
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+
+
+class TeacherNet(nn.Module):
+    def __init__(self):
+        super(TeacherNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.3)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        output = self.fc2(x)
+        return output
+
+
+def train_teacher(model, device, train_loader, optimizer, epoch):
+    model.train()
+    trained_samples = 0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        #
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+
+        trained_samples += len(data)
+        progress = math.ceil(batch_idx / len(train_loader) * 50)
+        print("\rTrain epoch %d: %d/%d, [%-51s] %d%%" %
+              (epoch, trained_samples, len(train_loader.dataset),
+               '-' * progress + '>', progress * 2), end='')
+
+
+def test_teacher(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest: average loss: {:.4f}, accuracy: {}/{} ({:.0f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return test_loss, correct / len(test_loader.dataset)
+
+
+def teacher_main():
+    epochs = 10
+    batch_size = 2
+    torch.manual_seed(0)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./dataset', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./dataset', train=False, download=True, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=1000, shuffle=True)
+
+    model = TeacherNet().to(device)
+    optimizer = torch.optim.Adadelta(model.parameters())
+
+    teacher_history = []
+
+    for epoch in range(1, epochs + 1):
+        train_teacher(model, device, train_loader, optimizer, epoch)
+        loss, acc = test_teacher(model, device, test_loader)
+
+        teacher_history.append((loss, acc))
+
+    torch.save(model.state_dict(), "teacher.pt")
+    return model, teacher_history
+
+
+teacher_model, teacher_history = teacher_main()
